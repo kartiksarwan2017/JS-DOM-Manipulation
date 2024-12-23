2e7525689657f79233b65f22c40040f581e31f5b
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-12-23 22:33:31 +0530

    Modify Text

---
+++ CreatingElements/index.js
@@ -0,0 +1,16 @@
+// Creating Elements
+
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+
+
+// Addding Element
+ul.append(li);
+
+// modifying text
+// const firstListItem = document.querySelector(".list-items");
+// console.log(firstListItem.innerText);
+// console.log(firstListItem.textContent);
+// console.log(firstListItem.innerHTML);
+
+
