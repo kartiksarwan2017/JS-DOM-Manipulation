878827720362998f4b33f8921a6313447646d3d7
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-12-23 22:13:24 +0530

    Selecting Elements in DOM

---
+++ index.js
@@ -0,0 +1,30 @@
+// DOM Manipulation
+
+
+// GetElementById
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+
+// GetElementByClassName()
+
+// const listItem = document.getElementsByClassName('list-items');
+// console.log(listItem);
+
+
+// getElementsByTagName()
+
+// const listItem = document.getElementsByTagName('li');
+// console.log(listItem);
+
+
+// querySelector()
+// const container = document.querySelector("div");
+// console.log(container);
+
+
+// querySelectorAll()
+
+/* Return the nodelist of collections of all the matching selector in this case its div */
+// const containers = document.querySelectorAll("div");
+// console.log(containers);
