fcd379c64b1d2f6211bc52f4714fa609862cf7c5
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-12-27 09:06:45 +0530

    Javascript Quote Generator - Changes in array

--- Project01/index.js
+++ Project01/index.js
@@ -5,31 +5,32 @@ let quote = document.querySelector(".quote");
 let person = document.querySelector(".person");
 
 const gitaQuotes = [
-    {
-      quote: "कर्मण्येवाधिकारस्ते मा फलेषु कदाचन। \nमा कर्मफलहेतुर्भूर्मा ते संगोऽस्त्वकर्मणि॥",
-      speaker: "Lord Krishna"
-    },
-    {
-      quote: "न जायते म्रियते वा कदाचिन् \nनायं भूत्वा भविता वा न भूयः। \nअजो नित्यः शाश्वतोऽयं पुराणो \nन हन्यते हन्यमाने शरीरे॥",
-      speaker: "Lord Krishna"
-    },
-    {
-      quote: "यथा दीपो निवातस्थो नेङ्गते सोपमा स्मृता। \nयोगिनो यतचित्तस्य युञ्जतो योगमात्मनः॥",
-      speaker: "Lord Krishna"
-    },
-    {
-      quote: "युक्ताहारविहारस्य युक्तचेष्टस्य कर्मसु। \nयुक्तस्वप्नावबोधस्य योगो भवति दुःखहा॥",
-      speaker: "Lord Krishna"
-    },
-    {
-      quote: "चञ्चलं हि मनः कृष्ण प्रमाथि बलवद् दृढम्। \nतस्याहं निग्रहं मन्ये वायोरिव सुदुष्करम्॥",
-      speaker: "Arjuna"
-    },
-    {
-      quote: "समदुःखसुखं धीरं सोमृतत्वाय कल्पते।",
-      speaker: "Lord Krishna"
-    }
-  ];
+  {
+    quote: "You have the right to perform your prescribed duties, but you are not entitled to the fruits of your actions.",
+    speaker: "Lord Krishna"
+  },
+  {
+    quote: "The soul is neither born, and nor does it die.",
+    speaker: "Lord Krishna"
+  },
+  {
+    quote: "When meditation is mastered, the mind is unwavering like the flame of a lamp in a windless place.",
+    speaker: "Lord Krishna"
+  },
+  {
+    quote: "A person is said to be elevated in yoga when, having renounced all material desires, he neither acts for sense gratification nor engages in fruitive activities.",
+    speaker: "Lord Krishna"
+  },
+  {
+    quote: "The mind is restless and difficult to restrain, but it is subdued by practice.",
+    speaker: "Lord Krishna"
+  },
+  {
+    quote: "A person who is not disturbed by happiness and distress and is steady in both is certainly eligible for liberation.",
+    speaker: "Lord Krishna"
+  }
+];
+
 
   btn.addEventListener("click", function(e) {
     let random = Math.floor(Math.random() * gitaQuotes.length);
